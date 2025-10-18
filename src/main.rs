fa3f6e3f6baff435d5bad90391aa84c0ab5ea0d2
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-18 17:11:12 +0545

    config: started template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use actix_web::{self, App, HttpServer, get};
+use askama::Template;
+use std::io;
+
+#[get("/")]
+async fn index() -> impl actix_web::Responder {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("🚀 Starting server at http://127.0.0.1:8080/");
+    HttpServer::new(move || App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
