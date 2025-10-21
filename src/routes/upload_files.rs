f1cf571a362f2e7949199f768040ac292ceeb574
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-21 19:24:20 +0545

    that comma where did it came from

--- src/routes/upload_files.rs
+++ src/routes/upload_files.rs
@@ -43,5 +43,5 @@ pub async fn upload(MultipartForm(form): MultipartForm<UploadForm>) -> impl Resp
     if let Some(file) = form.file.file_name {
         println!("File name: {file:?}");
     }
-    HttpResponse::Ok().body(format!("File '{}' uploaded successfully ", file_name,))
+    HttpResponse::Ok().body(format!("File '{}' uploaded successfully ", file_name))
 }
