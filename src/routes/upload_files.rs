bb21aa6c6b1e79c3ec91129ef3ff7b84a541bbe2
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-22 14:47:33 +0545

    refactor: there is already async methods in the docs so (rtfm)

--- src/routes/upload_files.rs
+++ src/routes/upload_files.rs
@@ -4,47 +4,82 @@ use crate::Responder;
 use crate::UploadForm;
 use crate::env;
 use crate::fs;
-use crate::io;
 use crate::post;
 
 #[post("/upload")]
 pub async fn upload(MultipartForm(form): MultipartForm<UploadForm>) -> impl Responder {
     let start = std::time::Instant::now();
-    let tmp_dir = env::temp_dir();
+    let tmp_dir = env::temp_dir().join("crane-rs");
 
-    if let Err(e) = fs::create_dir_all(tmp_dir.join("crane-rs")) {
+    if let Err(e) = fs::create_dir_all(&tmp_dir) {
         eprintln!("Failed to create directory: {e}");
         return HttpResponse::InternalServerError().body("Failed to create directory");
     }
 
-    let file_name = if let Some(file_name) = &form.file.file_name {
-        file_name.clone()
-    } else {
-        "uploaded_file".to_string()
-    };
+    let file_name = form
+        .file
+        .file_name
+        .clone()
+        .unwrap_or_else(|| "uploaded_file".to_string());
+    let file_path = tmp_dir.join(&file_name);
 
-    let file_path = tmp_dir.join("crane-rs").join(&file_name);
-
-    let mut f = match fs::File::create(&file_path) {
-        Ok(file) => file,
-        Err(e) => {
-            eprintln!("Failed to create file: {e}");
-            return HttpResponse::InternalServerError().body("Failed to create file");
+    // Move the temp file to the target location
+    match form.file.file.persist(&file_path) {
+        Ok(_) => {
+            println!(
+                "File stored: {:?}, write time: {:?}",
+                file_name,
+                start.elapsed()
+            );
+            HttpResponse::Ok()
+                .content_type("text/html")
+                .body(format!("File '{}' uploaded successfully", file_name))
         }
-    };
-
-    let mut temp_file = form.file.file;
-
-    if let Err(e) = io::copy(&mut temp_file, &mut f) {
-        eprintln!("Failed to write file: {e}");
-        return HttpResponse::InternalServerError().body("Failed to write file");
+        Err(e) => {
+            eprintln!("Failed to move file: {e}");
+            HttpResponse::InternalServerError().body("Failed to move file")
+        }
     }
-
-    if let Some(file) = form.file.file_name {
-        println!("File stored: {file:?}, write time: {:?}", start.elapsed());
-    }
-
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(format!("File '{}' uploaded successfully ", file_name))
 }
+
+// #[post("/upload")]
+// pub async fn upload(MultipartForm(form): MultipartForm<UploadForm>) -> impl Responder {
+//     let start = std::time::Instant::now();
+//     let tmp_dir = env::temp_dir();
+//
+//     if let Err(e) = fs::create_dir_all(tmp_dir.join("crane-rs")) {
+//         eprintln!("Failed to create directory: {e}");
+//         return HttpResponse::InternalServerError().body("Failed to create directory");
+//     }
+//
+//     let file_name = if let Some(file_name) = &form.file.file_name {
+//         file_name.clone()
+//     } else {
+//         "uploaded_file".to_string()
+//     };
+//
+//     let file_path = tmp_dir.join("crane-rs").join(&file_name);
+//
+//     let mut f = match fs::File::create(&file_path) {
+//         Ok(file) => file,
+//         Err(e) => {
+//             eprintln!("Failed to create file: {e}");
+//             return HttpResponse::InternalServerError().body("Failed to create file");
+//         }
+//     };
+//
+//     let mut temp_file = form.file.file;
+//
+//     if let Err(e) = io::copy(&mut temp_file, &mut f) {
+//         eprintln!("Failed to write file: {e}");
+//         return HttpResponse::InternalServerError().body("Failed to write file");
+//     }
+//
+//     if let Some(file) = form.file.file_name {
+//         println!("File stored: {file:?}, write time: {:?}", start.elapsed());
+//     }
+//
+//     HttpResponse::Ok()
+//         .content_type("text/html")
+//         .body(format!("File '{}' uploaded successfully ", file_name))
+// }
