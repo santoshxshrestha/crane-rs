8c345fd91f3f1c7d23c9bc85576ae7e5ca1b647a
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-13 11:52:00 +0545

    middleware set

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,14 +1,26 @@
+use crate::cli;
+use actix_web::HttpResponse;
 use actix_web::error::Error;
 use actix_web::middleware::Next;
 use actix_web::{
     body::MessageBody,
     dev::{ServiceRequest, ServiceResponse},
 };
+use clap::Parser;
+use cli::Args;
 
 pub async fn check_auth(
     req: ServiceRequest,
-    next: Next<impl MessageBody>,
+    next: Next<actix_web::body::BoxBody>,
 ) -> Result<ServiceResponse<impl MessageBody>, Error> {
-    println!("hit the middleware");
-    next.call(req).await
+    let args = Args::parse();
+    match args.get_auth() {
+        Some(_) => {
+            let res = HttpResponse::Found()
+                .append_header(("Location", "/login"))
+                .finish();
+            return Ok(req.into_response(res));
+        }
+        None => next.call(req).await,
+    }
 }
